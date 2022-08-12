4881f071b8ce036fa7e119876582d29c0eebe71b
Jonathan Humphrey <humphreyj1999@gmail.com>
2022-08-12 15:03:05 -0500

    initial commit

---
+++ src/components/Card.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="wrapper">
+       <div class="header">
+        <slot name="header"></slot>
+       </div> 
+       <div class="body">
+        <slot name="body"></slot>
+       </div>
+       <div class="footer">
+        <slot name="footer"></slot>
+       </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+.wrapper{
+    background-color: white;
+    box-shadow: 0 4px 8px rgba(255, 255, 255, 0.4);
+    color: black;
+}
+h1{
+    color: green;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,32 @@
+<script setup>
+import Card from "../components/Card.vue"
+</script>
+
+<template>
+  <div class="wrapper">
+    <Card>
+      <template v-slot:header>
+        <h1>Test</h1>
+      </template>
+    </Card>  
+    <Card>
+      <template v-slot:body>
+        <p>this is a string of text in the body of the card component</p>
+      </template>
+    </Card> 
+    <Card>
+      <template v-slot:footer>
+        <ul>
+          <li>list 1</li>
+          <li>list 2</li>
+        </ul>
+      </template>
+    </Card> 
+  </div>
+</template>
+
+<style scoped>
+p{
+  color: green;
+}
+</style>
